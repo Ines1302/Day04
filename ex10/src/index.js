fdf4beeb9db7fdaf733b49a2aa5d652b111c6bbd
Ines1302 <seeking.knowledge13@gmail.com>
2021-03-11 12:07:08 +0100

    String variables

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main () {
+    
+    var myFirstName = "Ines";
+    var myLastName = "Alagic";
+    var myHomeTown = "Sarajevo";
+
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    }; 
+  
+}
+
+console.log(main());
+
+module.exports = main;
