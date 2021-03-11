0ed46dd2fe8547d925ff4846d6081593a98eebfa
Ines1302 <seeking.knowledge13@gmail.com>
2021-03-11 23:31:30 +0100

    Decimal numbers

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,13 +1,11 @@
-function main () {
-    
-    var myDecimal = 8.2;
-    var myDecimalE = 254e-5;
+function main() {
+  var myDecimal = 8.2;
+  var myDecimalE = 254e-5;
 
-    return {
-        myDecimal, 
-        myDecimalE
-    }
-
+  return {
+    myDecimal,
+    myDecimalE,
+  };
 }
 
 console.log(main());
